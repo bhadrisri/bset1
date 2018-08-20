bd98b4c1e4451df216806e37a7a9772b56cfc94b
bhadrisri <42263855+bhadrisri@users.noreply.github.com>
2018-08-20 11:46:49 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int n3=sc.nextInt();
+		if((n1>n2)&&(n1>n3))
+		{
+		System.out.println("n1 is largest");
+		}
+		else if((n2>n1)&&(n2>n3))
+		{
+			System.out.println("n2 is largest");
+		}
+		else
+		{
+			System.out.println("n3 is largest");
+		}
+	}
+}
